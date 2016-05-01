2195a983234732645d320e017f850864e190254c
Mathieu Benoit <mathieu.benoit@cern.ch>
2016-05-01 08:47:07 +0200

    added last 24h plot to gui

--- plotter/plotter.py
+++ plotter/plotter.py
@@ -7,7 +7,7 @@ def convertTime(day,time):
 	date_object =datetime.datetime(day.year, day.month, day.day) + time
 	return mktime(date_object.timetuple())
 
-def UpdatePlots(grT,grpH):
+def UpdatePlots(grT,grpH,grT24,grpH24):
 #gROOT.SetStyle("Pub")
 
     db = MySQLdb.connect("localhost", "root", "aquarium", "AQUARIUMdb")
@@ -18,15 +18,24 @@ def UpdatePlots(grT,grpH):
 
     curs.execute(sqlcmd)
     data=curs.fetchall()
-
-
+    
+    sqlcmd = "SELECT * FROM tempdat WHERE TIMESTAMP(date, time)  >= (now() - INTERVAL 1 DAY) ;"
+    curs.execute(sqlcmd)
+    data24 = curs.fetchall()
+    #print data24
+    
     grT .Set(len(data))	
     grpH.Set(len(data))
-
+    
+    grT24 .Set(len(data24))	
+    grpH24.Set(len(data24))
+    formattedData_pH = [[],[]]    
     for i,col in enumerate(data) : 
 
         Time=convertTime(col[0],col[1])
         #print col[0],col[1],col[3],col[4],Time
+        formattedData_pH[0].append(Time)
+        formattedData_pH[1].append(col[4])       
         grT.SetPoint(i,Time,col[3])
         grpH.SetPoint(i,Time,col[4])
 
@@ -60,10 +69,48 @@ def UpdatePlots(grT,grpH):
     
     grT.SetTitle("Aquarium water temperature")
     grpH.SetTitle("Aquarium water pH")
+    formattedData24_pH = [[],[]]    
+    for i,col in enumerate(data24) : 
+
+        Time=convertTime(col[0],col[1])
+        #print col[0],col[1],col[3],col[4],Time
+        formattedData24_pH[0].append(Time)
+        formattedData24_pH[1].append(col[4])
+        grT24.SetPoint(i,Time,col[3])
+        grpH24.SetPoint(i,Time,col[4])
+
+    grT24.GetXaxis().SetTimeDisplay(1)
+    grpH24.GetXaxis().SetTimeDisplay(1)
+    grpH24.GetXaxis().SetNdivisions(8)
+    grT24.GetXaxis().SetNdivisions(8)
+    grT24.GetXaxis().SetTimeFormat("%d-%m-%Y %H:%M");
+    grpH24.GetXaxis().SetTimeFormat("%d-%m-%Y %H:%M");
+    grT24.GetXaxis().SetTimeOffset(3600)
+    grpH24.GetXaxis().SetTimeOffset(3600)
+
+    grT24.GetXaxis().SetLabelSize(0.02)
+    grpH24.GetXaxis().SetLabelSize(0.02)
+    grT24.GetYaxis().SetLabelSize(0.025)
+    grpH24.GetYaxis().SetLabelSize(0.025)
+
+    grT24.GetYaxis().SetTitleOffset(1.3)
+
+
+    grT24.SetMarkerSize(0.01)
+    grpH24.SetMarkerSize(0.01)
+
+    grT24.SetLineColor(2)
+    grpH24.SetLineColor(3)
+
+    grT24.GetYaxis().SetTitle("Temperature (C)")
+    grT24.GetYaxis().SetRangeUser(25,30)
+
+    grpH24.GetYaxis().SetTitle("pH")
     
+    grT24.SetTitle("Aquarium water temperature")
+    grpH24.SetTitle("Aquarium water pH")    
     
-    
-   # return grT,grpH
+    return formattedData_pH
 
     #can.Divide(2)
     #can.cd(1)
--- plotter/plotterdeamon.py
+++ plotter/plotterdeamon.py
@@ -10,13 +10,21 @@ server = THttpServer("""http:8080""")
 
 grT = TGraph(1000000)
 grpH = TGraph(1000000)
+grT24 = TGraph(1000000)
+grpH24 = TGraph(1000000)
+
 grT.SetName("Temperature")
 grpH.SetName("pH")
-UpdatePlots(grT,grpH)
+grT24.SetName("Temperature 24H")
+grpH24.SetName("pH 24H") 
+
+UpdatePlots(grT,grpH,grT24,grpH24)
 server.Register("/",grT)
 server.Register("/",grpH)
+server.Register("/",grT24)
+server.Register("/",grpH24)
 while 1 :
-	UpdatePlots(grT,grpH)	
+	UpdatePlots(grT,grpH,grT24,grpH24)	
 	sleep(60)
 	#print "Alive!!!!"
 
