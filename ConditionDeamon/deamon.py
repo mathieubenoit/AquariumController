4a1c80ad614115d7640fa6e785144d28b5274ebd
Mathieu Benoit <mathieu.benoit@cern.ch>
2016-05-01 08:46:27 +0200

    added averaging of T measure in deamon

--- ConditionDeamon/deamon.py
+++ ConditionDeamon/deamon.py
@@ -1,7 +1,7 @@
 from pyTemperature import *
 from pypH import *
 import MySQLdb
-
+from time import sleep
 
 db = MySQLdb.connect("localhost", "root", "aquarium", "AQUARIUMdb")
 curs=db.cursor()
@@ -10,8 +10,12 @@ curs=db.cursor()
 for i in range(1) :
 
 	pH=readpH()
-	Temp = readtemperature()
-
+	
+	Temp=0
+	for i in range(10):
+		Temp += readtemperature()
+		sleep(5)
+	Temp/=10	
 	print pH,Temp
 	sqlcmd ="""INSERT INTO tempdat (date,time,comments,temperature,pH,Relay1,Relay2) values(CURRENT_DATE(), NOW(), ' ',%f,%f,0,0)"""%(Temp,pH)
 	print sqlcmd
